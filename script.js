65ff819af08b0ff3d424a6cbf22ce67af340d899
HarryCoder-17 <128712701+HarryCoder-17@users.noreply.github.com>
2023-05-22 09:33:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**@type {HTMLCanvasElement} */
+
+quick_draw_data_set = [""]
 random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
 console.log(quick_draw_data_set[random_number]);
 sketch = quick_draw_data_set[random_number];
@@ -7,3 +10,32 @@ timer_check = "";
 draw_sketch = "";
 answer_holder = "";
 score = 0;
+
+function setup(){
+    canvas = createCanvas(280 , 280);
+    canvas.center();
+    background("white");
+}
+
+function draw(){
+
+}
+
+function check_sketch(){
+    timer_counter++;
+    document.getElementById("timer").innerHTML = "Timer" + timer_counter;
+    if(timer_counter > 400){
+        timer_counter = 0;
+        timer_check = "comepleted";
+    }
+    if(timer_check = "completed" || answer_holder == "set"){
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
+
+function updateCanvas(){
+    background("white");
+    Math.floor()
+}
